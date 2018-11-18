35dd2317ff2d918c49807d5e32f02724ab4ec34b
Juliano Rodrigues <juliano.silva.rodrigues@gmail.com>
2018-11-18 20:12:48 +0000

    Add eslint to the project and remove package.lock

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,35 @@
-const siteMeta = require('./site-config');
+const siteMeta = require('./site-config')
 
 module.exports = {
     siteMetadata: {
-        ... siteMeta
+        ...siteMeta,
     },
     plugins: [
-        `gatsby-plugin-react-helmet`,
-        `gatsby-transformer-sharp`,
-        `gatsby-plugin-sharp`,
+        'gatsby-plugin-react-helmet',
+        'gatsby-transformer-sharp',
+        'gatsby-plugin-sharp',
         {
-            resolve: `gatsby-source-graphql`,
+            resolve: 'gatsby-source-graphql',
             options: {
-                fieldName: `vicious`,
-                url: `https://api-euwest.graphcms.com/v1/cjoefozni7i7i01ght4oymxzy/master`,
-                typeName: `ViciousCMS`,
+                fieldName: 'vicious',
+                url:
+                    'https://api-euwest.graphcms.com/v1/cjoefozni7i7i01ght4oymxzy/master',
+                typeName: 'ViciousCMS',
                 refetchInterval: 60,
-            }
+            },
         },
         {
-            resolve: `gatsby-plugin-manifest`,
+            resolve: 'gatsby-plugin-manifest',
             options: {
                 name: 'Vicious Ink Tatto Shop',
                 short_name: 'VIT shop',
                 start_url: '/',
                 background_color: '#000',
                 theme_color: '#000',
-                display: 'minimal-ui'
-            }
+                display: 'minimal-ui',
+            },
         },
-        `gatsby-plugin-sass`,
-        `gatsby-plugin-offline`
-    ]
+        'gatsby-plugin-sass',
+        'gatsby-plugin-offline',
+    ],
 }
