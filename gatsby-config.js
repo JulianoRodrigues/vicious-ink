d4780b56c79b5d1eb7ac9e01547c15d5a64df50f
Juliano Rodrigues <juliano.silva.rodrigues@gmail.com>
2018-11-18 16:55:04 +0000

    Change gatsby-source-graphcms
    gatsby-source-graphcms because it's deprecated. Add gatsby-source-graphql

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,42 +9,12 @@ module.exports = {
         `gatsby-transformer-sharp`,
         `gatsby-plugin-sharp`,
         {
-            resolve: `gatsby-source-graphcms`,
+            resolve: `gatsby-source-graphql`,
             options: {
-                endpoint: `https://api-euwest.graphcms.com/v1/cjoefozni7i7i01ght4oymxzy/master`,
-                token: `eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ2ZXJzaW9uIjoxLCJ0b2tlbklkIjoiNWRmMmY2ZWItYTEyZC00OWEwLTk4ZWQtZmM3MmZiM2NhZmQyIn0.v2E7zeD2UAiK0r-73HyNlJcsno9YN5Paoozp1T2GFOA`,
-                query: `{
-                    allArtists: artists {
-                        id,
-                        name,
-                        specialized,
-                        photo {
-                            id
-                        }
-                    },
-                    allTattoos: tattoos {
-                        id,
-                        photo {
-                            id
-                        }
-                    },
-                    allCategories: categories {
-                        id,
-                        name
-                    },
-                    allConventions: conventions {
-                        id,
-                        name,
-                        description
-                    },
-                    allStores: stores {
-                        id,
-                        location,
-                        photo {
-                            id
-                        }
-                    }
-                }`
+                fieldName: `vicious`,
+                url: `https://api-euwest.graphcms.com/v1/cjoefozni7i7i01ght4oymxzy/master`,
+                typeName: `ViciousCMS`,
+                refetchInterval: 60,
             }
         },
         {
