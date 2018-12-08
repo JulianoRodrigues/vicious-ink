87b19cf3ef9b94da37736ae7a4dad1a7b446da53
Juliano Rodrigues <juliano.silva.rodrigues@gmail.com>
2018-12-08 20:37:25 +0000

    Create nodes for assets

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,24 +1,65 @@
-/**import { createRemoteFileNode } from 'gatsby-source-filesystem'
+const fetch = require('node-fetch')
+const { createRemoteFileNode } = require('gatsby-source-filesystem')
 
-exports.onCreateNode = ({ node, actions, store, cache }) => {
+exports.sourceNodes = async ({
+    actions,
+    createNodeId,
+    createContentDigest
+}) => {
     const { createNode } = actions
-    return new Promise((resolve, reject) => {
-        // download image and create a File node
-        // with gatsby-transformer-sharp and gatsby-plugin-sharp
-        // that node will become an ImageSharp
-        const fileNode = createRemoteFileNode({
-            url: node.url,
-            store,
-            cache,
-            createNode,
-            createNodeId: id => `image-sharp-${id}`
-        })
 
-        if (fileNode) {
-            // link File node to DogImage node
-            // at field image
-            node.image___NODE = fileNode.id
+    const processImage = image => {
+        const nodeId = createNodeId(`image-${image.fileName}`)
+        const nodeContent = JSON.stringify(image)
+
+        const nodeMeta = {
+            id: nodeId,
+            parent: null,
+            children: [],
+            internal: {
+                type: 'Image',
+                content: nodeContent,
+                contentDigest: createContentDigest(image)
+            }
         }
+
+        const node = Object.assign({}, image, nodeMeta)
+        return node
+    }
+
+    const response = await fetch(
+        'https://api-euwest.graphcms.com/v1/cjoefozni7i7i01ght4oymxzy/master?query={assets{fileName,url}}'
+    )
+
+    const {
+        data: { assets }
+    } = await response.json()
+
+    assets.forEach(asset => {
+        const nodeData = processImage(asset)
+        createNode(nodeData)
     })
+
+    return
 }
-*/
+
+exports.onCreateNode = async props => {
+    if (!props.node || props.node.internal.type !== 'Image') {
+        return
+    }
+
+    const { node, actions, store, cache } = props
+    const { createNode } = actions
+
+    const fileNode = await createRemoteFileNode({
+        url: node.url,
+        store,
+        cache,
+        createNode,
+        createNodeId: id => `image-${node.id}`
+    })
+
+    if (fileNode) {
+        node.image___NODE = fileNode.id
+    }
+}
