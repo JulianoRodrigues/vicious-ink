48e10f75a09309e213d802d1613916f6297f3dd8
Juliano Rodrigues <juliano.silva.rodrigues@gmail.com>
2019-02-16 23:25:01 +0000

    Starting Artists Grid

--- src/pages/artists.js
+++ src/pages/artists.js
@@ -3,6 +3,8 @@ import Helmet from 'react-helmet'
 import Img from 'gatsby-image'
 import { graphql, Link } from 'gatsby'
 import styled from 'styled-components'
+import remcalc from 'remcalc'
+import theme from '../styles/theme'
 
 import Layout from 'layouts/layout'
 import Nav from 'components/navbar'
@@ -11,6 +13,17 @@ import Footer from 'components/footer'
 
 const Section = styled.section`
     padding: 0px 24px 50px;
+`
+
+const ArtistGrid = styled.div`
+    display: grid;
+    grid-template-columns: 100%;
+    grid-row-gap: ${remcalc(150)};
+    @media only screen and (max-width: ${theme.responsive.large} ){
+        grid-template-columns: repeat(24,1fr);
+        grid-row-gap: ${remcalc(150)};
+        grid-column-gap: ${remcalc(32)};
+    }
 `
 
 const ArtistImage = styled(Img)`
@@ -32,6 +45,23 @@ const Artists = ({ data: { allContentfulArtist } }) => {
             <Main>
                 <Section className="mw9 center">
                     <div className="cf ph2-ns">
+                        <ArtistGrid>
+                            <div>
+                                cenas
+                            </div>
+                            <div>
+                                cenas
+                            </div>
+                            <div>
+                                cenas
+                            </div>
+                            <div>
+                                cenas
+                            </div>
+                            <div>
+                                cenas
+                            </div>
+                        </ArtistGrid>
                         <div className="fl w-100 l w-100 w-50-s w-25-l">
                             <div className="w-100">
                                 {allContentfulArtist &&
